7dcd2f6bf8cc2e2418f280b0f11356495c53a39c
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-10 22:28:23 +0000

    first component in react

---
+++ src/js/component/semaphor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Semaphor = () => {
+  return (
+    <div className="semaphor">
+      <div className="light red"></div>
+      <div className="light yellow"></div>
+      <div className="light green"></div>
+    </div>
+  );
+};
+
+export default Semaphor;
