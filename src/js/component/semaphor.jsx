bcf5b89a6f1d98f5b31c91bd8444863a00541bc8
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-10 22:57:11 +0000

    some styles fixes

--- src/js/component/semaphor.jsx
+++ src/js/component/semaphor.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 const Semaphor = () => {
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-4 text-center">
-          <div className="semaphor">
-            <div className="light red"></div>
-            <div className="light yellow"></div>
-            <div className="light green"></div>
-          </div>
-        </div>
+    <div className="semaphor-container">
+      <div className="pole"></div>
+      <div className="lights">
+        <div className="light red"></div>
+        <div className="light yellow"></div>
+        <div className="light green"></div>
       </div>
     </div>
   );
