2c5f5afd5c04c24194865bd0c42ca4957b257383
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-10 22:30:02 +0000

    adding bs to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Agregar Bootstrap CSS
+import './styles/styles.css';
+import Semaphor from './js/components/Semaphor';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Semaphor />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
