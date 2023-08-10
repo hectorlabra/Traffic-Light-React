62967f2c9c7e92952725a5809c61d36032e38d3b
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-10 23:01:00 +0000

    adding the light effect on react

--- src/js/component/semaphor.jsx
+++ src/js/component/semaphor.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './semaphor.css'; // Cambiar la ruta según la ubicación de tus estilos CSS
 
 const Semaphor = () => {
+  const [color, setColor] = useState(null);
+
+  const handleLightClick = (selectedColor) => {
+    setColor(selectedColor);
+  };
+
   return (
     <div className="semaphor-container">
       <div className="pole"></div>
       <div className="lights">
-        <div className="light red"></div>
-        <div className="light yellow"></div>
-        <div className="light green"></div>
+        <div
+          className={`light red ${color === 'red' ? 'on' : ''}`}
+          onClick={() => handleLightClick('red')}
+        ></div>
+        <div
+          className={`light yellow ${color === 'yellow' ? 'on' : ''}`}
+          onClick={() => handleLightClick('yellow')}
+        ></div>
+        <div
+          className={`light green ${color === 'green' ? 'on' : ''}`}
+          onClick={() => handleLightClick('green')}
+        ></div>
       </div>
     </div>
   );
